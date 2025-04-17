5d3208d258bdf0ce09f74c3c1879a5a817488fbb
M@teo22 <djokodev75@gmail.com>
2025-04-17 18:36:55 +0100

    Update ALLOWED_HOSTS in settings.py to include localhost and 127.0.0.1 for local development

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-r(!-c6!av^f9k9jrug@=307^c3$h^_69$r0#umemcui)bx(t+j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["blog.djokodev.com"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "blog.djokodev.com"]
 
 
 SITE_ID = 1
