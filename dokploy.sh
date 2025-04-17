1119c6e63cf1b1548c6ae510f81a9ca0f8844e00
M@teo22 <djokodev75@gmail.com>
2025-04-17 19:06:26 +0100

    Remove dokploy.sh script as it is no longer needed for deployment processes. This simplifies the project structure by eliminating an unused file.

--- dokploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# dokploy.sh - Script de démarrage pour Dokploy
-
-# Collecte des fichiers statiques
-python manage.py collectstatic --noinput
-
-# Démarrage de Gunicorn avec bind à 0.0.0.0
-exec gunicorn --bind 0.0.0.0:8000 --workers 3 mysite.wsgi:application
